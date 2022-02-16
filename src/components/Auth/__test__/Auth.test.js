00b8eed1046a575fe3b123344b74a0596072de1f
Chris DeCleene <chris_decleene@ymail.com>
2022-02-16 15:58:43 -0700

    Add initial RED test for checking empty inputs.

--- src/components/Auth/__test__/Auth.test.js
+++ src/components/Auth/__test__/Auth.test.js
@@ -1,6 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import Auth from "./Auth";
+import { MemoryRouter } from "react-router-dom";
+import Auth from "../Auth";
 
-test("renders Auth component", () => {
-  render(<Auth />);
+const setIsLoggedIn = () => {};
+
+describe("Auth", () => {
+  test("inputs should be initially empty", () => {
+    render(<Auth setIsLoggedIn={setIsLoggedIn} />, { wrapper: MemoryRouter });
+    // Email input should be empty
+    expect(screen.getByRole("textbox").value).toBe("");
+    // Password input should be empty
+    expect(screen.getByLabelText("Password").value).toBe("");
+  });
 });
