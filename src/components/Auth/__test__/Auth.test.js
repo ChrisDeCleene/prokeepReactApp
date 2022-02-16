ef4df3e0f624aba5ed2c59f0bdfa344719bb858d
Chris DeCleene <chris_decleene@ymail.com>
2022-02-16 16:10:28 -0700

    Add tests for userAction in React Testing Library. Typing into email or password input. GREEN

--- src/components/Auth/__test__/Auth.test.js
+++ src/components/Auth/__test__/Auth.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import Auth from "../Auth";
 
 const setIsLoggedIn = () => {};
@@ -14,5 +15,15 @@ describe("Auth", () => {
     expect(screen.getByRole("textbox").value).toBe("");
     // Password input should be empty
     expect(screen.getByLabelText("Password").value).toBe("");
+  });
+  test("should be able to type an email", () => {
+    const emailInputElement = screen.getByRole("textbox");
+    userEvent.type(emailInputElement, "eve.holt@reqres.in");
+    expect(emailInputElement.value).toBe("eve.holt@reqres.in");
+  });
+  test("should be able to type a password", () => {
+    const passwordInputElement = screen.getByLabelText("Password");
+    userEvent.type(passwordInputElement, "password!");
+    expect(passwordInputElement.value).toBe("password!");
   });
 });
