da51c5f5b0f42d5647eb64fdfd3e2bb76d80989b
Chris DeCleene <chris_decleene@ymail.com>
2022-02-16 16:04:06 -0700

    Move render for Auth Component into a beforeEach hook to reduce repeating over all tests.

--- src/components/Auth/__test__/Auth.test.js
+++ src/components/Auth/__test__/Auth.test.js
@@ -5,8 +5,11 @@ import Auth from "../Auth";
 const setIsLoggedIn = () => {};
 
 describe("Auth", () => {
-  test("inputs should be initially empty", () => {
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
     render(<Auth setIsLoggedIn={setIsLoggedIn} />, { wrapper: MemoryRouter });
+  });
+  test("inputs should be initially empty", () => {
     // Email input should be empty
     expect(screen.getByRole("textbox").value).toBe("");
     // Password input should be empty
