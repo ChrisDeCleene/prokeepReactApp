2a0444b3a54ef12bcefd45b2bfb4f363d76ca587
Chris DeCleene <chris_decleene@ymail.com>
2022-02-16 17:26:11 -0700

    Add navigate to useEffect to redirect user to login page if not sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
@@ -7,6 +7,7 @@ import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate()
 
   useEffect(() => {
     // Check for token and if expiresIn has not expired
@@ -21,8 +22,9 @@ function App() {
       localStorage.removeItem("token");
       localStorage.removeItem("expiresIn");
       setIsLoggedIn(false);
+      navigate('/login')
     }
-  }, []);
+  }, [navigate]);
 
   return (
     <Layout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
